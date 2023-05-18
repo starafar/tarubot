e2d95f600b8bc35532cd3c679ffbd64e835edc99
Justin Coffman <jcoffman@techsecu.red>
2023-05-18 15:36:30 -0400

    Config template builder.

--- build.js
+++ build.js
@@ -1,5 +1,23 @@
-import { cpSync } from "fs";
+import { promises as fsPromises } from "fs";
+
+async function copyFileWithSkip(sourcePath, destinationPath, linesToSkip) {
+  try {
+    const sourceData = await fsPromises.readFile(sourcePath, "utf8");
+    const lines = sourceData.split("\n").slice(linesToSkip);
+    const newData = lines.join("\n");
+    await fsPromises.writeFile(destinationPath, newData, "utf8");
+    console.log(
+      `File copied successfully from ${sourcePath} to ${destinationPath}`
+    );
+  } catch (error) {
+    console.error("An error occurred while copying the file:", error);
+  }
+}
+
+const sourcePath = "src/config/local.yaml.example";
+const destinationPath = "dist/config/local.yaml.example";
+const linesToSkip = 10;
 
 console.log("Copying config template to dist folder...");
-cpSync("src/config", "dist/config", { recursive: true });
+copyFileWithSkip(sourcePath, destinationPath, linesToSkip);
 console.log("Done!");
